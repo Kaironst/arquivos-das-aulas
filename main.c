24d254f97153072a0b2680466551f8eee75bdb67
Kaironst <pedronovak4@gmail.com>
2025-04-04 22:09:59 -0300

    Submissão inicial.
    	new file:   biblioteca_encadeada.c
    	new file:   biblioteca_encadeada.h
    	new file:   main
    	new file:   main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "biblioteca_encadeada.h"
+#include "biblioteca_encadeada.c"
+
+int main() {
+
+    Biblioteca b;
+
+    inicializarBiblioteca(&b);
+
+    cadastrarLivro(&b, "Capitães da Areia", "Jorge Amado", 1937, "3333333333333");
+
+    cadastrarLivro(&b, "Dom Casmurro", "Machado de Assis", 1899, "4444444444444");
+
+    listarTodosLivros(&b);
+
+    emprestarLivro(&b, "3333333333333", "João", "04/04/2025");
+
+    listarTodosLivros(&b);
+
+    destruirBiblioteca(&b);
+
+return 0;
+
+}
